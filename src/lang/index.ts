9fd4960ac61ba808eb4cd0440da83ae45a48ca8c
Damien Zhao <zdm65477730@126.com>
2026-04-06 14:31:49 +0800

    enable Chinese lang
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -251,7 +251,7 @@ function getMarkdown(langPath: string) {
 
 // Make sure everything is exported as BCP 47 compatible codes
 // let enabledLanguages = ['en-US'].concat(['nl-NL', 'pt-BR','fr-FR','de-DE','zh-CN'].sort());
-let enabledLanguages = ["en-US"];
+let enabledLanguages = ["en-US", "zh-CN", "zh-TW"];
 let langData = Object.fromEntries(
   enabledLanguages.map((x) => [
     x,
